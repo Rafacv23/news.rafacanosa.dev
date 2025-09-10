8b656459dd85cdabc1140c3610290157b594c569
Rafa <rafacv23@gmail.com>
2025-09-10 12:27:28 +0200

    refactor(utils): clean up comments in buildSanityImgUrl function

--- apps/web/app/lib/utils.ts
+++ apps/web/app/lib/utils.ts
@@ -1,7 +1,7 @@
 export function buildSanityImgUrl(ref: string) {
   const baseUrl = "https://cdn.sanity.io/images/"
-  const projectId = "d5wqbyn6" // replace with your Sanity project ID
-  const dataset = "production" // replace with your dataset name
+  const projectId = "d5wqbyn6"
+  const dataset = "production"
 
   // ref format: image-<imageId>-<width>x<height>-<format>
   const match = ref.match(/^image-([a-f0-9]+)-(\d+x\d+)-(\w+)$/)
